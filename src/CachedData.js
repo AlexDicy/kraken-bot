73de72d0493c800e99f46d21219d6ff52d96cbb7
Lorenzo Lapucci <halexercraft@gmail.com>
2021-02-26 23:08:08 +0100

    Show account info as a table using "blessed" package

--- src/CachedData.js
+++ src/CachedData.js
@@ -1,3 +1,12 @@
 export default class CachedData {
-  static equivalentBalance;
+  // in EUR
+  static equivalentBalance = 0;
+  // balance
+  static eur = 0;
+  static xbt = 0;
+  static eth = 0;
+  static ada = 0;
+
+  /** @type Order[] */
+  static orders = [];
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,70 @@
+import blessed from "neo-blessed";
 import KrakenClient from "./kraken.js";
 import CachedData from "./CachedData.js";
 import Order from "./Order.js";
-import {Type} from "./Enums.js";
 
 const key = "***REMOVED***";
 const secret = "***REMOVED***";
 const kraken = new KrakenClient(key, secret);
 
-// display user's balance
-//console.log(await kraken.api("Balance"));
+const screen = blessed.screen({smartCSR: true});
+const layout = blessed.layout({
+  parent: screen,
+  width: "100%",
+  height: "100%"
+});
+const infoTable = blessed.table({
+  parent: layout,
+  scrollable: true,
+  noCellBorders: true,
+  align: "left",
+  border: "line",
+  style: {
+    border: {
+      fg: "#0000ff"
+    },
+    header: {
+      fg: "#ccFFcc"
+    },
+    cell: {
+      fg: "#ccFFcc"
+    }
+  }
+});
+const ordersTable = blessed.table({
+  parent: layout,
+  scrollable: true,
+  noCellBorders: true,
+  border: "line",
+  style: {
+    border: {
+      fg: "#0000ff"
+    },
+    header: {
+      bold: true
+    },
+    cell: {
+      fg: "#ccFFcc"
+    }
+  }
+});
 
-kraken.api("TradeBalance", {asset: "EUR"}).then(resp => {
+kraken.api("Balance").then(resp => {
+  CachedData.eur = resp.result["ZEUR"];
+  CachedData.xbt = resp.result["XXBT"];
+  CachedData.eth = resp.result["XETH"];
+  CachedData.ada = resp.result["ADA"];
+  return kraken.api("TradeBalance", {asset: "EUR"});
+}).then(resp => {
   CachedData.equivalentBalance = resp.result.eb;
   return kraken.api("OpenOrders");
 }).then(resp => {
   let open = resp.result.open;
+  let orders = [];
   for (let id of Object.keys(open)) {
     let order = open[id];
 
-    console.log(Type.fromString("buy"));
-
-    let obj = new Order(
+    orders.push(new Order(
       id,
       order.descr.pair,
       order.descr.type,
@@ -29,39 +73,37 @@ kraken.api("TradeBalance", {asset: "EUR"}).then(resp => {
       order.descr.ordertype,
       order.status,
       new Date(order.opentm * 1000)
-    );
-    console.dir(obj, {depth: null});
+    ));
   }
+  CachedData.orders = orders;
 
-  /*'OXGME7-IQ5K5-TAKTQF': {
-    refid: null,
-      userref: 0,
-      status: 'open',
-      opentm: 1614264265.009,
-      starttm: 0,
-      expiretm: 0,
-      descr: {
-      pair: 'XBTEUR',
-        type: 'sell',
-        ordertype: 'limit',
-        price: '42341.5',
-        price2: '0',
-        leverage: 'none',
-        order: 'sell 0.00096252 XBTEUR @ limit 42341.5',
-        close: ''
-    },
-    vol: '0.00096252',
-      vol_exec: '0.00000000',
-      cost: '0.00000',
-      fee: '0.00000',
-      price: '0.00000',
-      stopprice: '0.00000',
-      limitprice: '0.00000',
-      misc: '',
-      oflags: 'fciq'
-  }*/
+  let info = [
+    ["Tot", removeTrailingZero(CachedData.equivalentBalance) + " €"],
+    ["EUR", removeTrailingZero(CachedData.eur)],
+    ["XBT", removeTrailingZero(CachedData.xbt)],
+    ["ETH", removeTrailingZero(CachedData.eth)],
+    ["ADA", removeTrailingZero(CachedData.ada)]
+  ];
+  infoTable.setData(info);
 
+  let data = [["Pair", "Type", "Order Type", "Volume", "Price", "Time"]];
+  for (let order of CachedData.orders) {
+    data.push([
+      order.pair,
+      order.type,
+      order.orderType,
+      order.volume,
+      order.price,
+      order.openTime.toLocaleString()
+    ]);
+  }
+  ordersTable.setData(data);
+  screen.render();
 });
 
 // get Ticker Info
 //console.log(await kraken.api("Ticker", {pair: "XXBTZEUR"}));
+
+function removeTrailingZero(value) {
+  return value.replace(/([0-9])[0]+$/, "$1");
+}
